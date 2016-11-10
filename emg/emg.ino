0b79e72877e6d295bd37e7316b06767862b94802
pringithub <pring@umich.edu>
2016-11-09 19:38:42 -0500

    updated emg.ino

--- emg/emg.ino
+++ emg/emg.ino
@@ -1,101 +1,84 @@
 /*
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+  This example code is in the public domain.
 */
-
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
+#define AVG_OR_NAH          1
+#define MOVING_WINDOW       1
+#define REP_THRESHHOLD_HIGH 2.5
+#define REP_THRESHHOLD_LOW  1.1
 
-#define AVG_OR_NAH 1
-#define MOVING_WINDOW 1
-#define REP_THRESHHOLD 0.35
-
-int isRep;
+int inRep=0;
 int repCount = 0;
-int lastAverage=-1;
+float lastAverage=-1;
+
+unsigned long pulseStart=0, pulseEnd=0, pulseWidth=0;
+unsigned long deadStart=0, deadEnd=0, deadWidth=0;
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-
-  if (AVG_OR_NAH == 0) {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-    float voltage = sensorValue * (5.0 / 1023.0);
-
-    // print out the value you read:
-    Serial.println(voltage);
-  }
-
-
-  else {
-    float timeslice[MOVING_WINDOW];
-    //get "moving" average
-    // ************
-    // |wndw |wndw |
-    for (int i = 0; i < MOVING_WINDOW; ++i) {
-      // read the input on analog pin 0:
-      int sensorValue = analogRead(A0);
-      // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-      float voltage = sensorValue * (5.0 / 1023.0);
-      timeslice[i] = voltage;
-    }
-
-    //get the average
-    float avg = 0;
-    for (int i = 0; i < MOVING_WINDOW; ++i) {
-      avg += timeslice[i];
-    }
-    avg /= MOVING_WINDOW;
-
-    
-
-    unsigned long timeRise=0, timeFall=0, deadStart=0, deadEnd=0;
-    unsigned long pulseWidth, deadWidth;
-
-    //edge detection for reps
-    if (avg >= REP_THRESHHOLD) {
-
-      //start of pulse
-      if (lastAverage == -1) {
-        timeRise = millis();
-        deadEnd = timeRise;
-        deadWidth = deadStart - deadEnd;
-        
-        repCount++;
-        Serial.println("repCount----------------------------------------");
-        Serial.println(repCount);        
-      }
-
-    }
-    else {//avg < REP_THRESHHOLD
-      
-      //end of pulse
-      if(lastAverage > -1) {        
-        timeFall = millis();        
-        pulseWidth = timeFall - timeRise;
-        deadStart = timeFall;
-      }
-      
-      avg = -1;
-    }
-
-    
-
-    //quantize the voltage
-    //voltage = (int)(voltage/0.4);
-
-    // print out the value you read:
-    Serial.println(avg);
-  }
-
+    float timeslice[MOVING_WINDOW];
+    //get "moving" average
+    // ************
+    // |wndw |wndw |
+    for (int i = 0; i < MOVING_WINDOW; ++i) {
+      // read the input on analog pin 0:
+      int sensorValue = analogRead(A0);
+      // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+      float voltage = sensorValue * (5.0 / 1023.0);
+      timeslice[i] = voltage;
+    }
+    //get the average
+    float avg = 0;
+    for (int i = 0; i < MOVING_WINDOW; ++i) {
+      avg += timeslice[i];
+    }
+    avg /= MOVING_WINDOW;
+   
+    //edge detection for reps
+    if (double(avg) >= REP_THRESHHOLD_HIGH) {
+      //start of pulse
+      if (lastAverage >= REP_THRESHHOLD_LOW) {
+        pulseStart = millis();
+        Serial.print("pulseStart: ");Serial.print(pulseStart);
+        deadWidth = (repCount > 0) ? pulseStart - pulseEnd : 0;    
+        //Serial.println(20);             
+      }      
+    }
+    //still avove threshholding levels
+    //if inRep, will keep going
+    //if !inRep, won't start 
+    else if(avg >= REP_THRESHHOLD_LOW) {}
+    else {//avg < REP_THRESHHOLD_LOW
+     
+      //end of pulse
+      if(lastAverage > -1) {        
+        pulseEnd = millis();
+        Serial.print("     pulseEnd: ");Serial.print(pulseEnd);
+        pulseWidth = pulseEnd - pulseStart;
+        Serial.print("      pulseWidth: ");Serial.println(pulseWidth);
+        //get rid of questionable reps
+        if(pulseWidth > 500) {
+          repCount++;
+//          Serial.println(10);
+        }
+      }
+      
+      avg = -1;      
+    }
+    lastAverage = avg;
+    //quantize the voltage
+    //voltage = (int)(voltage/0.4);
+    // print out the value you read:
+//    Serial.println(avg);
+  
 }
