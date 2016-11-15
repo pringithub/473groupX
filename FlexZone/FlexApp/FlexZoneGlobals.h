78bfd3d6d778ab97edf949789f7b5cbb74dc5b2d
Don <nguyen.donbui@gmail.com>
2016-11-15 16:36:58 -0500

    Testing project for Digipot & blinky

---
+++ FlexZone/FlexApp/FlexZoneGlobals.h
@@ -0,0 +1,39 @@
+/*
+* Application Name:		FlexZone (Application)
+* File Name: 			FlexZoneGlobals.h
+* Group: 				GroupX - FlexZone
+* Description:			Global data structures for inter-thread access.
+*/
+
+#ifndef FLEXZONE_GLOBALS_H
+#define FLEXZONE_GLOBALS_H
+
+//**********************************************************************************
+// Header Files
+//**********************************************************************************
+//SYS/BIOS Header Files
+#include <ti/sysbios/knl/Semaphore.h>
+
+//TI-RTOS Header Files
+#include <ti/drivers/PIN.h>
+#include <ti/drivers/UART.h>
+
+//**********************************************************************************
+// Required Definitions
+//**********************************************************************************
+
+
+//**********************************************************************************
+// Data Structures
+//**********************************************************************************
+
+//**********************************************************************************
+// Globally Scoped Variables (for RTOS: Semaphores, Mailboxes, Queues, Data Structures)
+//**********************************************************************************
+
+
+//**********************************************************************************
+// General Functions
+//**********************************************************************************
+
+#endif /* FLEXZONE_GLOBALS_H */
